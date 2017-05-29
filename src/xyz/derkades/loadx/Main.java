7584fc81be9075ec99eaedd282999a172ff1aed8
RobinMC <robinslot@hotmail.nl>
2017-05-29 21:37:54 +0200

    Complete recode for version 2.0

--- src/xyz/derkades/loadx/Main.java
+++ src/xyz/derkades/loadx/Main.java
@@ -3,7 +3,7 @@ package xyz.derkades.loadx;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.bukkit.GameMode;
+import org.bukkit.Chunk;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -15,92 +15,77 @@ import net.md_5.bungee.api.ChatColor;
 
 public class Main extends JavaPlugin {
 	
-	//Sorry for one class code :(
+	private static final int CHUNKS_EVERY_TICK = 30;
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
-		if (label.equalsIgnoreCase("generate")){
-			if (args.length != 2){
+		if (command.getName().equalsIgnoreCase("generate")){
+			if (args.length != 1){
 				return false;
 			}
 			
 			if (sender instanceof Player){
 				Player player = (Player) sender;
-				Location playerLocation = player.getLocation();
+				Location loc = player.getLocation();
 				
 				int blocks = Integer.parseInt(args[0]);
-				int interval = Integer.parseInt(args[1]); 
 				
-				final List<Location> teleportLocations = new ArrayList<Location>();
-				// Add all locations to a list, so we can teleport the player to all locations later.
-				for (int x = -blocks; x < blocks + 1; x += 50){
-					for (int z = -blocks; z < blocks + 1; z += 50){
-						Location location = new Location(
-								playerLocation.getWorld(), 
-								playerLocation.getX() + x, 
-								100, 
-								playerLocation.getZ() + z,
-								0,
-								90);
-						teleportLocations.add(location);
+				player.sendMessage(ChatColor.BLUE + "Calculating chunk coordinates...");
+				
+				//Unfortunately I had to use a Block list instead of Chunk list, because bukkit apperently can't handle that many chunks.
+				List<Location> chunkBlocks = new ArrayList<>();
+				
+				for (int x = -blocks; x < blocks; x += 16){
+					for (int y = 0; y < loc.getWorld().getMaxHeight(); y += 16){
+						for (int z = -blocks; z < blocks; z+= 16){
+							Location chunkLoc = new Location(player.getWorld(), loc.getX() + x, y, loc.getZ() + z);
+							chunkBlocks.add(chunkLoc);
+						}
 					}
 				}
 				
-				player.sendMessage(ChatColor.AQUA + "Teleporting will begin in 10 seconds. Please look at the ground and do not move. Feel free to have a drink and come back later.");
-				player.sendMessage(ChatColor.BLUE + "This will take approximately " + ((teleportLocations.size() * interval) / 20) / 60 + " minutes.");
-				// Look down
-				playerLocation.setPitch(90);
-				player.teleport(playerLocation);
+				final int totalChunks = chunkBlocks.size();
 				
-				final GameMode originalGameMode = player.getGameMode();
-				player.setGameMode(GameMode.SPECTATOR);
+				player.sendMessage(ChatColor.BLUE + "Done! " + totalChunks + " chunks found.");
+				player.sendMessage(ChatColor.AQUA + "Chunk generation will start in a few seconds! Please note that it may take a long time and will probably cause a bit of lag.");				
 				
-				
-				
-				// Now teleport the player to all locations with delay
 				new BukkitRunnable(){
 					
-					boolean sendTitle = false;
+					int passedCount = 0;
 					
-					@SuppressWarnings("deprecation")
-					public void run(){
-						if (teleportLocations.isEmpty()){
-							player.sendMessage(ChatColor.GREEN + "" + ChatColor.BOLD + "Chunk generating complete!");
-							player.sendMessage(ChatColor.GRAY + 
-									"You should now restart your server, because due to a bug, Minecraft may "
-									+ "keep some chunks loaded. We want these chunks to be generated so "
-									+ "they load faster or show up on a map, but we don't want chunks to be loaded when "
-									+ "no one is nearby. This pointlessly uses up RAM.");
-							player.teleport(playerLocation); //Teleport back to original location
-							player.setGameMode(originalGameMode);
-							player.sendTitle("", "");
+					public void run(){						
+						if (chunkBlocks.size() == 0){ //If there are no chunks left to generate
+							player.sendMessage(ChatColor.GREEN + "" + ChatColor.BOLD + "Chunk loading complete.");
 							this.cancel();
 							return;
 						}
 						
-						// Get the first location from the list, then remove the first, so the next time we get the first is a different location
-						Location teleportLocation = teleportLocations.get(0);
-						teleportLocations.remove(0);
+						//Get chunks then remove chunks, the next time this is run there are new chunks to generate.
 						
-						player.teleport(teleportLocation);
-						
-						int timeLeft = (teleportLocations.size() * interval) / 20; //Divide by 20 because interval is in ticks
-						
-						// The sendTitle boolean thing is to only send the title every other time.
-						if (sendTitle){
-							player.sendTitle("Teleporting...", "ETA: " + timeLeft + " seconds.");
-							sendTitle = false;
-						} else {
-							sendTitle = true;
+						for (int i = 0; i <= CHUNKS_EVERY_TICK; i++){
+							passedCount++;
+							
+							if (chunkBlocks.size() == 0){
+								return; //Return for now, next run it will be catched by the code above.
+							}
+							
+							Chunk chunk = chunkBlocks.get(0).getChunk();
+							chunkBlocks.remove(0);
+							chunk.load(true); //Generate chunk
+							chunk.unload(); //Unload chunk to save RAM
 						}
+						
+						int remaining = totalChunks - passedCount;
+						player.sendMessage(ChatColor.DARK_AQUA + "Generating chunks... " + ChatColor.DARK_GRAY + "" + ChatColor.BOLD + "|" + ChatColor.AQUA + " Chunks remaining: " + remaining);
 					}
-				}.runTaskTimer(this, 10*20, interval);
+				}.runTaskTimer(this, 5*20, 1);
 				return true;
 			} else {
 				sender.sendMessage("You must be a player in order to execute this command, because I can't teleport a console around the map.");
+				return false;
 			}
+		} else {
+			return false;
 		}
-		return false;
 	}
-
 }
